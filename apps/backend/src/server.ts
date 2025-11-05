320f4c242dfe7ee97f8caf96d3aa7df2002a7d21
kevinmistrele <kevintavaresmistrele@gmail.com>
2025-11-04 21:51:50 -0300

    fix: add comment to clarify server initialization in server.ts

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-
+// Inicia o servidor na porta especificada no arquivo .env ou na porta 4000 por padrão
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
