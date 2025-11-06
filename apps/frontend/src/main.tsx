6298cde6640a026fc4c60276517d0c11518e4eb4
kevinmistrele <kevintavaresmistrele@gmail.com>
2025-11-06 08:54:33 -0300

    refactor: remove StrictMode wrapper from main render

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import {createRoot} from "react-dom/client"
 import "./styles/global.css"
 import App from "./App.tsx"
 import {AuthProvider} from "./contexts/AuthContext.tsx";
-import { Toaster } from "sonner"
+import {Toaster} from "sonner"
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
         <AuthProvider>
             <App />
             <Toaster
@@ -24,5 +22,4 @@ createRoot(document.getElementById("root")!).render(
                 }}
             />
         </AuthProvider>
-    </StrictMode>,
 )
